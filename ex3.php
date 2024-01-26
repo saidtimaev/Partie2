da1cc80f63022562c13c6eddb4153ed4b203a6ca
saidtimaev <said.timaev@stagiaire-elan.fr>
2024-01-26 10:04:19 +0100

    ex3 commenté

--- ex3.php
+++ ex3.php
@@ -11,5 +11,6 @@ dans un nouvel onglet (_blank).
 <p>Affichage :
 </p> 
 
-
+<!-- l'attribut indique l'adresse du lien et l'attribut target permet de dire de quelle manière on va l'ouvrir
+Ici la valeur _blank permet d'ouvrir le lien dans un nouvel onglet -->
 <a href="https://elan-formation.fr/" target="_blank">Elan Formation</a>
